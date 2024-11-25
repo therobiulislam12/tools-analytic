aa6d5068de91a6471a1c1265ac10a2601f510a9d
Robiul Islam <therobiulislam12@gmail.com>
2024-11-26 01:11:51 +0600

    added nodemodules

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => '8794cee6bada23941e30');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => '93981e30d16ab2bc02e7');
